e232f4d40a9edcbc9ad47b44d328e3835dc609e1
Dovi KAPLAN <dkaplan@c4r24s3.wethinkcode.co.za>
2019-06-03 15:04:36 +0200

    add output if no args

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,16 +6,16 @@ int   main(int ac, char **av) {
   int i = 1;
   int j = 0;
 
+  if (ac == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  }
   while (i < ac) {
     while (j < (int)strlen(av[i])) {
       std::cout << (char)toupper(av[i][j]);
       j++;
     }
-    if (i != ac - 1)
-      std::cout << (' ');
-    else
-      std::cout << std::endl;
     i++;
     j = 0;
   }
+  std::cout << std::endl;
 }
